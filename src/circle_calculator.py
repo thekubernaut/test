65149175e4e7aef6fa3248b4533b334b9c403cad
Louis Weston <lw@glasskube.com>
2025-04-21 12:15:14 -0500

    feat: add circle circumference calculation function

--- src/circle_calculator.py
+++ src/circle_calculator.py
@@ -10,4 +10,16 @@ def calculate_circle_area(radius):
     Returns:
         float: The area of the circle
     """
-    return math.pi * radius ** 2 
+    return math.pi * radius ** 2
+
+def calculate_circle_circumference(radius):
+    """
+    Calculate the circumference of a circle given its radius.
+    
+    Args:
+        radius (float): The radius of the circle
+        
+    Returns:
+        float: The circumference of the circle
+    """
+    return 2 * math.pi * radius 
